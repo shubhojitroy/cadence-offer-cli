7fa24ae932614a39cfd6d609f734f215197953ad
shubhojit <royshubhojit@gmail.com>
2018-09-16 08:58:49 +1000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'co-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cadence-offer-cli';
+}
