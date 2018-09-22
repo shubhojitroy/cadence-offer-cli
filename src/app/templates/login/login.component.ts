fea121e12b20dcdda40baf69ac4945d937fa7951
shubhojit <royshubhojit@gmail.com>
2018-09-22 20:32:37 +1000

    update component login content and some processes.

--- src/app/templates/login/login.component.ts
+++ src/app/templates/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'co-login',
   templateUrl: './login.component.html',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  pageTitle = 'Login to lodge the application';
+  loginform: FormGroup;
+  login: FormControl;
+  constructor(private router: Router) { }
+
+  createFormControls() {
+    this.login = new FormControl('', Validators.required);
+  }
+
+  loginForm() {
+    this.loginform = new FormGroup ({
+      login: this.login
+    });
+  }
 
   ngOnInit() {
+    this.createFormControls();
+    this.loginForm();
+  }
+  onSubmit() {
+    this.router.navigate(['/application']);
   }
 
 }
